56e6015da32ea3f12b00e8f11bd8e5db5854e7d6
Trevor Smith <tsmit019@hammer.cs.ucr.edu>
2017-05-10 16:48:45 -0700

    added exit.sh

---
+++ tests/exit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#tests exit and commands with exit
+
+echo "-----This is the start of the exit_tests-----"
+
+RSHELL=../bin/rshell
+
+$RSHELL << EOF
+ls -al
+echo hello world && exit
+ech hello wrolsjdsa || exit
+exit
+EOFs
+
